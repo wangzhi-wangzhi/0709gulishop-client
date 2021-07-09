bd8831bcc17eb0ea7991a94c63115ca25f903c83
wangzhi <546758862@qq.com>
2021-07-09 11:37:22 +0800

    first

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+// import App from './App.vue'
+import App from '@/App'//@是一个别名，是一个小名， 代表就是我们的src路径
+
+Vue.config.productionTip = false
+
+new Vue({
+  el:'#app',
+  render: h => h(App),
+})
+
